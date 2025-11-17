0be1f56c42f1501634e08e7ab5c8d16977a52f54
naveenkarthi007 <naveenkarthi.mn@gmail.com>
2025-11-17 11:52:02 +0530

    second1  commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/loginpage-react-">
     <App />
   </BrowserRouter>
 );
